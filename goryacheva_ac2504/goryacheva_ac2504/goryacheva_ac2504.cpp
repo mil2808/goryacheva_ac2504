a54213b99c5d274f5241a4b4b0fc972a31d91ff8
User <User@HOME-PC>
2026-09-12 20:59:06 +0400

    добавила структуру трубы,  компрессорной станции и меню

--- goryacheva_ac2504/goryacheva_ac2504/goryacheva_ac2504.cpp
+++ goryacheva_ac2504/goryacheva_ac2504/goryacheva_ac2504.cpp
@@ -1,7 +1,30 @@
 ﻿#include <iostream>
+#include <string>
+using namespace std; 
 
+struct Pipe 
+{
+    string name_p;
+    int diam;
+    double lenght;
+};
+
+struct CS
+{
+    string name_cs;
+    int amount_ws;
+    int amount_ws_in_prog;
+    string class_cs;
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "1. add pipe\n";
+    cout << "2. add cs\n";
+    cout << "3. view all objects\n";
+    cout << "4. edit pipe\n";
+    cout << "5. edit cs\n";
+    cout << "6. save\n";
+    cout << "7. load\n";
+    cout << "0. exit\n";
 }
 
