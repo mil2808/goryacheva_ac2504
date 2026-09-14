4a7ca3297898aae8bc3d97e8e33cad28e052248e
User <User@HOME-PC>
2026-09-15 02:07:34 +0400

    написала проверки для ввода данных

--- goryacheva_ac2504/goryacheva_ac2504/goryacheva_ac2504.cpp
+++ goryacheva_ac2504/goryacheva_ac2504/goryacheva_ac2504.cpp
@@ -7,6 +7,7 @@ struct Pipe
     string name_p;
     int diam;
     double lenght;
+    bool repair;
 };
 
 struct CS
@@ -16,7 +17,88 @@ struct CS
     int amount_ws_in_prog;
     string class_cs;
 };
-int main()
+
+bool exist_pipe = false;
+bool exist_cs = false;
+Pipe pipe1;
+CS CS1;
+
+int inputint(string text)
+{
+    int value;
+    while (true)
+    {
+        cout << text;
+        cin >> value;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "not number.\n";
+        }
+        else
+        {
+            cin.ignore(1000, '\n');
+            return value;
+
+        }
+
+    } 
+}
+
+double inputdouble(string text)
+{
+    double value;
+    while (true)
+    {
+        cout << text;
+        cin >> value;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "not number. \n";
+        }
+        else
+        {
+            cin.ignore(1000, '\n');
+            return value;
+        }
+    }
+}
+
+string inputstrind(string text)
+{
+    string value;
+    while (true);
+    {
+        cout << text;
+        getline(cin, value);
+        if (value == "")
+        {
+            cout << "Cannot be empty. \n";
+        }
+        else
+        {
+            return value;
+        }
+    }
+}
+
+bool inputyesno(string text)
+{
+    string value;
+    while (true)
+    {
+        cout << text << " y/n ";
+        getline(cin, value);
+        if (value == "y") return true;
+        if (value == "n")  return false;
+        cout << "Please type y or n.\n";
+    }
+}
+
+void menu()
 {
     cout << "1. add pipe\n";
     cout << "2. add cs\n";
@@ -26,5 +108,10 @@ int main()
     cout << "6. save\n";
     cout << "7. load\n";
     cout << "0. exit\n";
+}
+
+int main()
+{
+
 }
 
